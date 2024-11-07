b8cc697235641a217c8d98eeecf88069474db1b1
Alex Stampekis <alex.stabekis@gmail.com>
2024-11-07 02:42:55 +0000

    Modularize the cipher project by defining the functions in separate source files, while declaring them in separate header files. Using CMake to build everything up.

---
+++ src/MPAGSCipher/ProcessCommandLine.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "ProcessCommandLine.hpp"
+using namespace std;
+
+bool processCommandLine(const vector<string>& args,
+    bool& helpRequested, bool& versionRequested,
+    string& inputFileName, string& outputFileName){
+    
+     // Status flag to indicate whether or not the parsing was successful
+    bool processStatus{true};
+
+    // Process command line arguments - ignore zeroth element, as we know this
+    // to be the program name and don't need to worry about it
+    size_t nargs=args.size();
+    for (size_t i{1}; i < nargs; ++i) {
+        if (args[i] == "-h" || args[i] == "--help") {
+            helpRequested = true;
+        } else if (args[i] == "-v" || args[i] == "--version") {
+            versionRequested = true;
+        } else if (args[i] == "-i") {
+            // Handle input file option
+            // Next element is filename unless "-i" is the last argument
+            if (i == nargs - 1) {
+                cerr << "[error] -i requires a filename argument"
+                          << std::endl;
+                // exit main with non-zero return to indicate failure
+                processStatus=false;
+                break;
+            } else {
+                // Got filename, so assign value and advance past it
+                inputFileName = args[i + 1];
+                ++i;
+            }
+        } else if (args[i] == "-o") {
+            // Handle output file option
+            // Next element is filename unless "-o" is the last argument
+            if (i == nargs - 1) {
+                std::cerr << "[error] -o requires a filename argument"
+                          << std::endl;
+                // exit main with non-zero return to indicate failure
+                processStatus=false;
+                break;
+            } else {
+                // Got filename, so assign value and advance past it
+                outputFileName = args[i + 1];
+                ++i;
+            }
+        } else {
+            // Have an unknown flag to output error message and return non-zero
+            // exit status to indicate failure
+            std::cerr << "[error] unknown argument '" << args[i]
+                      << "'\n";
+            processStatus=false;
+            break;
+        }
+    }
+    return processStatus;
+}
+//===end of cmd line parsing function===
---
+++ src/MPAGSCipher/ProcessCommandLine.hpp
@@ -0,0 +1,10 @@
+#ifndef MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+#define MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+
+#include <string>
+#include <vector>
+
+bool processCommandLine(const std::vector<std::string>& ,
+    bool& , bool& , std::string& , std::string& );
+
+#endif
